649a1af549de1d48b589cc98a56b06a97d16aa32
Isma90 <ismavx90@gmail.com>
2022-06-02 18:07:20 -0400

    API update  - Add health api - Update base path

--- act1/src/main/java/org/acme/GreetingResource.java
+++ act1/src/main/java/org/acme/GreetingResource.java
@@ -5,12 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/greeting")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("/health")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String healthCheck(){
+        return "I'm Ok! :D";
     }
 }
